09b6e74271e576b7c51056871061b37a4a7862a6
Priyanshu Priyadarshi <priyadarshi6982@gmail.com>
2025-01-06 02:47:25 +0530

    deployed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -78,4 +78,15 @@ router.post('/login', async (req, res) => {
     }
   });
 
+  router.post('/logout', (req, res) => {
+    try {
+        // Clear the token cookie
+        res.clearCookie('token', { httpOnly: true, secure: false });
+        res.status(200).json({ message: 'Logged out successfully' });
+    } catch (err) {
+        console.error('Error during logout:', err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
 module.exports = router;
