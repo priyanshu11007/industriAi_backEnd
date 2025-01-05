516b5129482b47fce2978a1445a37c4e09eb4093
priyanshu11007 <priyadarshi6982@gmail.com>
2025-01-06 03:45:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const cookieParser = require('cookie-parser');
 dotenv.config();
 
 const app = express();
-app.use(cors({credentials: true}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());  // This middleware parses incoming JSON requests
 app.use(cookieParser());  // This middleware parses cookies
